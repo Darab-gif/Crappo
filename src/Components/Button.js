9ac7c7d7a098464e2cd45dd0f2b57432848dea89
Darab <darabcode@gmail.com>
2022-09-04 18:56:14 +0100

    Initiated crappo project

---
+++ src/Components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ type }) => {
+  return (
+    <div>
+      <button className="bg-carolina_blue">{type}</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      steel_blue: "hsl(218, 33%, 77%)",
+      royal_blue: "hsl(225, 68%, 53%)",
+      flickr_blue: "hsla(225, 68%, 53%, 0.3)",
+      carolina_blue: "hsl(201, 92%, 47%)",
+      oxford_blue: "hsl(218, 70%, 18%)",
+      oxford_blue_2: "hsl(217, 100%, 12%)",
+      oxford_blue_3: "hsl(218, 71%, 11%)",
+      gainsboro_50: "hsla(0, 0%, 85%, 0.5)",
+      lavender_web: "hsl(225, 67%, 91%)",
+      sonic_silver: "hsl(0, 0%, 47%)",
+      light_gray: "hsl(0, 0%, 84%)",
+      cultured: "hsl(0, 0%, 97%)",
+      black_10: "hsla(0, 0%, 0%, 0.1)",
+      black_8: "hsla(0, 0%, 0%, 0.08)",
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
+      jet: "hsl(0, 0%, 20%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
